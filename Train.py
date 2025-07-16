bc55b8fee226d1ad5a7bbd39df1749e75d47d216
Dipolelo778 <dipolelomohlala323@gmail.com>
2025-07-16 03:38:24 +0200

    Update Train.py

--- Train.py
+++ Train.py
@@ -1,34 +1,58 @@
 # train.py
 
 import torch
-from torch import nn, optim
+import torch.nn as nn
+import torch.optim as optim
 from loader import VITONDataset
 from torch.utils.data import DataLoader
-from network import VITONHD
+
+from segmentation import SegmentationNet
+from pose_estimator import PoseEstimator
+from gmm import GMM
+from refiner import RefinerUNet
+from inpainting import Inpainter
+
 from config import Config
 
 dataset = VITONDataset(Config.DATA_ROOT)
 loader = DataLoader(dataset, batch_size=Config.BATCH_SIZE, shuffle=True)
 
-model = VITONHD()
-optimizer = optim.Adam([
-    {'params': model.segmentation.parameters()},
-    {'params': model.cloth_parser.parameters()},
-    {'params': model.gmm.parameters()},
-    {'params': model.refiner.parameters()},
-    {'params': model.inpainter.parameters()},
-], lr=Config.LR)
+segmentation = SegmentationNet().to(Config.DEVICE)
+pose_estimator = PoseEstimator()
+gmm = GMM().to(Config.DEVICE)
+refiner = RefinerUNet().to(Config.DEVICE)
+inpainter = Inpainter().to(Config.DEVICE)
 
-loss_fn = nn.L1Loss()
+optimizer = optim.Adam(
+    list(segmentation.parameters()) +
+    list(gmm.parameters()) +
+    list(refiner.parameters()) +
+    list(inpainter.parameters()),
+    lr=Config.LR
+)
+
+l1_loss = nn.L1Loss()
 
 for epoch in range(Config.EPOCHS):
     for person, cloth in loader:
-        output = model.refiner(torch.cat([person, cloth], dim=1))
-        loss = loss_fn(output, person)
+        person = person.to(Config.DEVICE)
+        cloth = cloth.to(Config.DEVICE)
+
+        mask = segmentation(person)
+        pose = pose_estimator(person)
+        pose = pose.to(Config.DEVICE)
+
+        warped_cloth = gmm(person, pose, mask, cloth)
+        refined = refiner(torch.cat([person, warped_cloth], dim=1))
+        final_output = inpainter(refined)
+
+        loss = l1_loss(final_output, person)
 
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
+
+        print(f"Epoch {epoch} Loss: {loss.item()}")
 
 
 
